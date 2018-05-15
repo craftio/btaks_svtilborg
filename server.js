7c74b9d6bf20b99616fcc8ee2d4b489ac0599aea
btaks <btaks@avans.nl>
2018-05-15 22:54:00 +0200

    Attempt at fixing Cannot GET / in Heroku.

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const port = process.env.PORT || config.webPort;
 
 // Setup parser and faults.
 app.use(bodyParser.json());
+
+// API section.
+app.use('/api', require('./routes/api'));
+
 app.all('*', (req, res, next) => {
     console.log(req.method + " on " + req.url); // Stefan, moet je nog testen.
     next();                                     // Goto next
 });
-
-// API section.
-app.use('/api', require('./routes/api'));
 
 // Setup port using port variable.
 app.listen(port, () => {
