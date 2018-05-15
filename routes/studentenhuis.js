9866878e12fdae97315718b257d3149e4b7d7248
Stefan van Tilborg <stefan131@outlook.com>
2018-05-15 21:50:34 +0200

    Studentenhuis.js volledig afgewerkt, op delete na (deze duurt niet lang, zal morgen gemaakt worden) Verder moet alles nog nagelopen worden. Er is een grote kans dat studentenhuis niet zal werken aangezien Björn de werking van de server omgegooid heeft (waardoor deze er veel beter en professioneler uit ziet :) )

--- routes/studentenhuis.js
+++ routes/studentenhuis.js
@@ -66,6 +66,42 @@ router.post("/", function(req, res) {
     });
 });
 
+router.get("/", function(req, res) {
+    const id  = req.params.id || '';
+
+    let query = "SELECT `studentenhuis`.`ID`, `studentenhuis`.`Naam`, `studentenhuis`.`Adres`, `user`.`Voornaam`, `user`.`Email`" +
+        "FROM `studentenhuis`" +
+        "JOIN `user`" +
+        "ON `user`.`ID` = `studentenhuis`.`userID`";
+
+    con.query(query, function (err, result) {;
+        if (err) throw err;
+        if (result !== undefined && result[0] !== undefined) {
+            let outputJson = {};
+            let i = 0;
+            result.forEach(function(item) {
+                let recvID = item.ID;
+                let recvNaam = item.Naam;
+                let recvAdres = item.Adres;
+                let recvContact = item.Voornaam;
+                let recvEmail = item.Email;
+                var dataAdd = {"ID" : recvID, "naam" : recvNaam, "adres" : recvAdres, "contact" : recvContact, "email" : recvEmail};
+                outputJson[i] = dataAdd;
+                i++;
+            });
+            res.status(200)
+            res.json(outputJson);
+        } else {
+            res.status(412);
+            res.json({
+                "message": "Onbekende fout",
+                "code": 0,
+                "datetime": dateTime.create().format('Y-m-d H:M:S')
+            });
+        }
+    })
+});
+
 router.get("/:id?", function(req, res) {
     const id  = req.params.id || '';
 
@@ -75,27 +111,83 @@ router.get("/:id?", function(req, res) {
         "ON `user`.`ID` = `studentenhuis`.`userID`" +
         "WHERE `studentenhuis`.`ID` = " + id;
 
-        con.query(query, function (err, result) {
-            console.log("test2");
-            if (err) throw err;
-            if (result !== undefined && result[0] !== undefined) {
-                result = result[0];
-                let recvID = result.ID;
-                let recvNaam = result.Naam;
-                let recvAdres = result.Adres;
-                let recvContact = result.Voornaam;
-                let recvEmail = result.Email;
-                res.status(200);
-                res.json({"ID" : recvID, "naam" : recvNaam, "adres" : recvAdres, "contact" : recvContact, "email" : recvEmail});
-            } else {
-                res.status(412);
+    con.query(query, function (err, result) {
+        console.log("test2");
+        if (err) throw err;
+        if (result !== undefined && result[0] !== undefined) {
+            result = result[0];
+            let recvID = result.ID;
+            let recvNaam = result.Naam;
+            let recvAdres = result.Adres;
+            let recvContact = result.Voornaam;
+            let recvEmail = result.Email;
+            res.status(200);
+            res.json({"ID" : recvID, "naam" : recvNaam, "adres" : recvAdres, "contact" : recvContact, "email" : recvEmail});
+        } else {
+            res.status(412);
+            res.json({
+                "message": "Niet gevonden (huisId bestaat niet)",
+                "code": 0,
+                "datetime": dateTime.create().format('Y-m-d H:M:S')
+            });
+        }
+    })
+});
+
+router.put("/:id?", function(req, res) {
+    const id  = req.params.id || '';
+
+    let checkQuery = "SELECT `user`.`Email`" +
+        "FROM `studentenhuis`" +
+        "JOIN `user`" +
+        "ON `user`.`ID` = `studentenhuis`.`userID`" +
+        "WHERE `studentenhuis`.`ID` = " + id;
+
+    con.query(checkQuery, function (err, result) {
+        if (err) throw err;
+        if (result === undefined || result[0] === undefined) {
+            res.status(412);
+            res.json({
+                "message": "Niet gevonden (huisId bestaat niet)",
+                "code": 0,
+                "datetime": dateTime.create().format('Y-m-d H:M:S')
+            });
+        } else {
+            result = result[0];
+            let userEmail = result.Email;
+            let email = jwt.decode(req.header('X-Access-Token'), Buffer(config.secretKey)).sub;
+            if (email != userEmail) {
+                res.status(409);
                 res.json({
-                    "message": "Niet gevonden (huisId bestaat niet)",
+                    "message": "Conflict (Gebruiker mag deze data niet wijzigen)",
                     "code": 0,
                     "datetime": dateTime.create().format('Y-m-d H:M:S')
                 });
+            } else {
+                let naam = req.body.naam || null;
+                let adres = req.body.adres || null;
+                if (naam === null || adres === null) {
+                    res.status(412);
+                    res.json({
+                        "message": "Een of meer properties in de request body ontbreken of zijn foutief",
+                        "code": 0,
+                        "datetime": dateTime.create().format('Y-m-d H:M:S')
+                    });
+                } else {
+                    let query = "UPDATE `studentenhuis`" +
+                        "SET `studentenhuis`.`Naam` = '" + naam + "', `studentenhuis`.`Adres` = '" + adres + "'" +
+                        "WHERE `studentenhuis`.`ID` = " + id;
+
+                    con.query(query, function (err) {
+                        if (err) throw err;
+                        res.status(200)
+                        res.json({"temp" : "moet nog gemaakt worden"}); //HIER MOET NOG EEN QUERY OM ALLE INFO VAN HET GEWIJZIDGE STUDENTENHUIS TE SHOWEN
+                    });
+                }
             }
-        })
+
+        }
+        });
     });
 
 module.exports = router;
