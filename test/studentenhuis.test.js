c3ab0359db2a1960e8ec22a34a71f3cb2cb716c5
btaks <btaks@avans.nl>
2018-05-16 21:23:21 +0200

    Added studentenhuis tests and removed obsolete tests (Thank you Robin Schellius for the correction in Slack).

--- test/studentenhuis.test.js
+++ test/studentenhuis.test.js
@@ -9,10 +9,33 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('Studentenhuis API POST', () => {
+    let validToken = null;
+    let invalidToken = 0;
+    before(() => {
+        chai.request(server)
+            .post('api/login')
+            .send({
+                "email": "btaks@avans.nl",
+                "password": "btaks"
+            })
+            .end((err, res) => {
+                validToken = res.body.token
+            });
+    });
     it('should throw an error when using invalid JWT token', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .post('/api/studentenhuis')
+            .set('X-Access-Token', invalidToken)
+            .send({
+                "email": "btaks@avans.nl",
+                "password": "btaks"
+            })
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
         done();
     });
 
@@ -20,6 +43,16 @@ describe('Studentenhuis API POST', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .post('/api/studentenhuis')
+            .set('X-Access-Token', validToken)
+            .send({
+                "naam": "Breda de gekste",
+                "adres": "Breda"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
         done();
     });
 
@@ -27,6 +60,15 @@ describe('Studentenhuis API POST', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .post('/api/studentenhuis')
+            .set('X-Access-Token', validToken)
+            .send({
+                "adres": "Breda"
+            })
+            .end((err, res) => {
+                res.should.have.status(412);
+            });
         done();
     });
 
@@ -34,15 +76,43 @@ describe('Studentenhuis API POST', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .post('/api/studentenhuis')
+            .set('X-Access-Token', validToken)
+            .send({
+                "naam": "Breda de gekste"
+            })
+            .end((err, res) => {
+                res.should.have.status(412);
+            });
         done();
     });
 });
 
 describe('Studentenhuis API GET all', () => {
+    let validToken = null;
+    let invalidToken = 0;
+    before(() => {
+        chai.request(server)
+        .post('/api/login')
+        .send({
+            "email": "btaks@avans.nl",
+            "password": "btaks"
+        })
+        .end((err, res) => {
+            validToken = res.body.token;
+        });
+    });
     it('should throw an error when using invalid JWT token', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .get('/api/studentenhuis')
+            .set('X-Access-Token', invalidToken)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
         done();
     });
 
@@ -50,15 +120,40 @@ describe('Studentenhuis API GET all', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .get('/api/studentenhuis')
+            .set('X-Access-Token', validToken)
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
         done();
     });
 });
 
 describe('Studentenhuis API GET one', () => {
+    let validToken = null;
+    let invalidToken = 0;
+    before(() => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "btaks@avans.nl",
+                "password": "btaks"
+            })
+            .end((err, res) => {
+                validToken = res.body.token;
+            });
+    });
     it('should throw an error when using invalid JWT token', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .get('/api/studentenhuis/1')
+            .set('X-Access-Token', invalidToken)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
         done();
     });
 
@@ -66,6 +161,12 @@ describe('Studentenhuis API GET one', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .get('/api/studentenhuis/1')
+            .set('X-Access-Token', validToken)
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
         done();
     });
 
@@ -73,15 +174,44 @@ describe('Studentenhuis API GET one', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .get('/api/studentenhuis/9999')
+            .set('X-Access-Token', validToken)
+            .end((err, res) => {
+                res.should.have.status(404);
+            });
         done();
     });
 });
 
 describe('Studentenhuis API PUT', () => {
+    let validToken = null;
+    let invalidToken = 0;
+    before(() => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "btaks@avans.nl",
+                "password": "btaks"
+            })
+            .end((err, res) => {
+                validToken = res.body.token;
+            });
+    });
     it('should throw an error when using invalid JWT token', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .put('/api/studentenhuis/1')
+            .set('X-Access-Token', invalidToken)
+            .send({
+                "naam": "Lovensdijk",
+                "adres": "Lovensdijkstraat, Breda"
+            })
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
         done();
     });
 
@@ -89,6 +219,16 @@ describe('Studentenhuis API PUT', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .put('/api/studentenhuis/1')
+            .set('X-Access-Token', invalidToken)
+            .send({
+                "naam": "Lovensdijk",
+                "adres": "Lovensdijkstraat, Breda"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
         done();
     });
 
@@ -96,6 +236,15 @@ describe('Studentenhuis API PUT', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .put('/api/studentenhuis/1')
+            .set('X-Access-Token', invalidToken)
+            .send({
+                "adres": "Lovensdijkstraat, Breda"
+            })
+            .end((err, res) => {
+                res.should.have.status(404);
+            });
         done();
     });
 
@@ -103,36 +252,44 @@ describe('Studentenhuis API PUT', () => {
         //
         // Hier schrijf je jouw testcase.
         //
+        chai.request(server)
+            .put('/api/studentenhuis/1')
+            .set('X-Access-Token', invalidToken)
+            .send({
+                "adres": "Lovensdijkstraat, Breda"
+            })
+            .end((err, res) => {
+                res.should.have.status(404);
+            });
         done();
     });
 });
 
 describe('Studentenhuis API DELETE', () => {
+    let validToken = null;
+    let invalidToken = 0;
+    before(() => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "btaks@avans.nl",
+                "password": "btaks"
+            })
+            .end((err, res) => {
+                validToken = res.body.token;
+            });
+    });
+
     it('should throw an error when using invalid JWT token', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
-        done();
-    });
-
-    it('should return a studentenhuis when posting a valid object', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done();
-    });
-
-    it('should throw an error when naam is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done();
-    });
-
-    it('should throw an error when adres is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
+        chai.request(server)
+            .del('/api/studentenhuis/97')
+            .set('X-Access-Token', invalidToken)
+            .end((err, res) => {
+                res.should.have.status(401);
+            });
         done();
     });
 });
